63421649a76f5235e4abc9a172a4fd17385eb5bd
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-05-07 16:42:57 -0700

    added the adding movies to nav functionality

--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path ="/" component={MovieList}/>
-        <Route path="/movies/:id" component={Movie}/>
+        <Route path="/" exact component={MovieList}/>
+        <Route path="/movies/:id" render={(props) => <Movie {...props} addToSavedList={this.addToSavedList}/>}/>
       </div>
     );
   }
